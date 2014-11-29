07825c98556dbcc065419343b3eab1d14fbfd773
Bevaz <The.Bevaz@gmail.com>
2014-11-29 20:00:00 +0300

    Cura profile generation Few more settings

--- generate.py
+++ generate.py
@@ -3,17 +3,29 @@ from jinja2 import Template
 Filament = {'ABS', 'PLA'}
 Quality = {'HighQuality', 'FastSpeed'}
 Model = {'FDMi1', 'FDMi2'}
+Apps = {'Simplify3D', 'Cura'}
 
-tempSimplify3D = 'Simplify3D/zbot_templ.fff'
-saveSimplify3D = 'Simplify3D/Zbot_{0}_{1}_{2}.fff'
+templ = {}
+save = {}
+templData = {}
+templFile = {}
 
-with open (tempSimplify3D, "r") as templFileSimplify3D:
-    templDataSimplify3D=Template(templFileSimplify3D.read())
+templ[ 'Simplify3D' ] = 'Simplify3D/zbot_templ.fff'
+save[ 'Simplify3D' ] = 'Simplify3D/Zbot_{0}_{1}_{2}.fff'
+
+templ[ 'Cura' ] = 'Cura/zbot_templ.ini'
+save[ 'Cura' ] = 'Cura/Zbot_{0}_{1}_{2}.ini'
+
+for app in Apps:
+    with open (templ[ app ], "r") as templFile[ app ]:
+        templData[ app ]=Template(templFile[ app ].read())
 
 for filament in Filament:
      for quality in Quality:
           for model in Model:
 
+               nozzleSize = 0.4
+               layerHeight = 0.24
                printTempABS = 250
                printTempPLA = 210
                defaultSpeedHQ = 15
@@ -25,18 +37,45 @@ for filament in Filament:
                printSizeY = 150
                printSizeZ = 140
                infillPercentage = 30
+               perimeterOutlines = 3
+               solidLayers = 4
 
+               args = {}
+               args[ 'Simplify3D' ] = {
+                    'modelName' : model,
+                    'layerHeight' : layerHeight,
+                    'nozzleSize' : nozzleSize,
+                    'width' : nozzleSize * 1.2,
+                    'perimeterOutlines' : perimeterOutlines,
+                    'solidLayers' : solidLayers,
+                    'defaultSpeed' : int(defaultSpeedHQ * 60) if quality == 'HighQuality' else int(defaultSpeedFS * 60),
+                    'maxXYSpeed' : int(maxXYSpeed * 60),
+                    'maxZSpeed' : int(maxZSpeed * 60),
+                    'printSizeX' : printSizeXi1 if model == 'FDMi1' else printSizeXi2,
+                    'printSizeY' : printSizeY,
+                    'printSizeZ' : printSizeZ,
+                    'firstLayerTemp' :  printTempABS + 5 if filament == 'ABS' else printTempPLA + 5,
+                    'otherLayerTemp' : printTempABS if filament == 'ABS' else printTempPLA,
+                    'infillPercentage' : infillPercentage,
+                                      }
 
-               Simplify3DArgs = { 'defaultSpeed' : int(defaultSpeedHQ * 60) if quality == 'HighQuality' else int(defaultSpeedFS * 60),
-                                  'maxXYSpeed' : int(maxXYSpeed * 60),
-                                  'maxZSpeed' : int(maxZSpeed * 60),
-                                  'printSizeX' : printSizeXi1 if model == 'FDMi1' else printSizeXi2,
-                                  'printSizeY' : printSizeY,
-                                  'printSizeZ' : printSizeZ,
-                                  'firstLayerTemp' :  printTempABS + 5 if filament == 'ABS' else printTempPLA + 5,
-                                  'otherLayerTemp' : printTempABS if filament == 'ABS' else printTempPLA,
-                                  'infillPercentage' : infillPercentage,
+               args[ 'Cura' ] = {
+                    'modelName' : model,
+                    'layerHeight' : layerHeight,
+                    'nozzleSize' : nozzleSize,
+                    'wallThickness' : round(perimeterOutlines * nozzleSize, 1 ),
+                    'solidLayerThickness' : round(solidLayers * layerHeight, 1),
+                    'defaultSpeed' : defaultSpeedHQ if quality == 'HighQuality' else defaultSpeedFS,
+                    'maxXYSpeed' : maxXYSpeed,
+                    'maxZSpeed' : maxZSpeed,
+                    'printSizeX' : printSizeXi1 if model == 'FDMi1' else printSizeXi2,
+                    'printSizeY' : printSizeY,
+                    'printSizeZ' : printSizeZ,
+                    'firstLayerTemp' :  printTempABS + 5 if filament == 'ABS' else printTempPLA + 5,
+                    'otherLayerTemp' : printTempABS if filament == 'ABS' else printTempPLA,
+                    'infillPercentage' : infillPercentage,
                                 }
 
-               with open ( saveSimplify3D.format( model, quality, filament ), "w" ) as saveFile:
-                    saveFile.write( templDataSimplify3D.render(Simplify3DArgs) )
+               for app in Apps:
+                    with open ( save[ app ].format( model, quality, filament ), "w" ) as saveFile:
+                         saveFile.write( templData[ app ].render( args[ app ] ) )
